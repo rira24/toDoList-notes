7021184abc1945a15de7f5ac4aa451bd5eab9a71
Rithika <rithu427@gmail.com>
2023-07-14 13:50:15 -0500

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
